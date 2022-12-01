ca10df1f260e616d3adfce2531dd94fa8f46054e
Antony Choi <antony.choi@oocl.com>
2022-12-01 14:51:46 +0800

    test: TDD first test

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_the_number__when_countingOff_given_number_is_not_mutiple_of_3_5_7() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String actual = fizzBuzz.countingOff(1);
+        //then
+        assertEquals("1", actual);
+    }
+    
 }
